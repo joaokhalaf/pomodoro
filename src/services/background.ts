a97b663b23e09dbe11c29d61f90605f9a6cd75db
Joao <232938876+joaokhalaf@users.noreply.github.com>
2025-09-30 22:23:31 -0300

    fixing bg's

--- src/services/background.ts
+++ src/services/background.ts
@@ -4,26 +4,26 @@ export const backgroundImages: BackgroundImage[] = [
   {
     id: "1",
     url: "https://w.wallhaven.cc/full/5y/wallhaven-5yd6d5.png",
-    name: "City",
+    name: "Tokyo City",
   },
   {
     id: "2",
     url: "https://w.wallhaven.cc/full/1k/wallhaven-1k5793.jpg",
-    name: "Lofi Bart",
+    name: "Bart on top of the van drinking coffee",
   },
   {
     id: "3",
-    url: "https://w.wallhaven.cc/full/kx/wallhaven-kxv8d7.png",
-    name: "Retro",
+    url: "https://w.wallhaven.cc/full/7j/wallhaven-7jgyre.jpg",
+    name: "Boy and Stars",
   },
   {
     id: "4",
     url: "https://w.wallhaven.cc/full/m3/wallhaven-m31l2y.jpg",
-    name: "Jinx and Vi",
+    name: "Boy on computer and dog sleeping",
   },
    {
     id: "5",
     url: "  https://w.wallhaven.cc/full/g7/wallhaven-g7z7gd.jpg",
-    name: "Jinx and Vi",
+    name: "Cat and Girl Sleeping",
   }
 ];
